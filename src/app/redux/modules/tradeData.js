0c48ac0dc67df5d904990de4dd9a25c690784f07
Will Vaughn <willieviseoae@gmail.com>
2016-02-21 10:53:50 -0800

    TDD the entire event dispatching cycle. There is definitely an abstaction to make to generalize this, for now since this is an exercise I'm not going to TDD the same thing for the other data requests because it should use essentially the same code and I will remove the extraction later and these tests I just wrote will inform that.

--- src/app/redux/modules/tradeData.js
+++ src/app/redux/modules/tradeData.js
@@ -1,5 +1,36 @@
+import { createAction } from 'redux-actions';
+
+// Constants
+export const TRADE_DATA_REQUEST = 'TRADE_DATA_REQUEST';
+export const TRADE_DATA_FAILURE = 'TRADE_DATA_FAILURE';
 export const TRADE_DATA_SUCCESS = 'TRADE_DATA_SUCCESS';
 
+// Actions
+const tradeDataFailure = createAction(TRADE_DATA_FAILURE);
+const tradeDataRequest = createAction(TRADE_DATA_REQUEST);
+const tradeDataSuccess = createAction(TRADE_DATA_SUCCESS, (tradeData) =>
+  ({ dataset: tradeData.dataset, lastUpdated: Date.now() })
+);
+
+export function fetchTradeData(comtrade, parameters) {
+  return (dispatch) => {
+    dispatch(tradeDataRequest());
+
+    function handleGetSuccess(tradeData) {
+      dispatch(tradeDataSuccess(tradeData));
+    }
+
+    function handleGetFailure(err) {
+      dispatch(tradeDataFailure(err));
+    }
+
+    return comtrade
+      .get(parameters)
+      .then(handleGetSuccess, handleGetFailure);
+  };
+}
+
+// Reducer
 const defaultState = { isFetching: false, dataset: [] };
 
 function storeTradeData(state, tradeData) {
--- src/app/redux/modules/tradeData.test.js
+++ src/app/redux/modules/tradeData.test.js
@@ -1,7 +1,133 @@
 import assert from 'assert';
-import tradeDataReducer, { TRADE_DATA_SUCCESS } from './tradeData';
+import { spy, stub, useFakeTimers } from 'sinon';
+import
+  tradeDataReducer,
+  {
+    TRADE_DATA_FAILURE,
+    TRADE_DATA_REQUEST,
+    TRADE_DATA_SUCCESS,
+    fetchTradeData
+  } from './tradeData';
 
 describe('app/redux/modules/tradeData', () => {
+  describe('fetchTradeData action', () => {
+    it('should manage event dispatching for successful trade data requests', (done) => {
+      // Using sinon fake timers to control the output of the date in the action
+      // Forcing javascript's internal clock to produce midnight on christmas.
+      const clock = useFakeTimers(new Date(2015, 11, 25).getTime());
+
+      // The data comes in from the comtrade api with some validation data, which
+      // will be ignored from the time being. The data relevant to the app, is the
+      // data stored in the "dataset" field of the tradeData results.
+      const comtradeData = { validation: 'test.validation', dataset: 'test.dataset' };
+
+      const comtrade = { get: stub() };
+      const parameters = { tradeRegime: 'test.tradeRegime', partnerAreas: 'test.partnerAreas' };
+      const dispatch = spy();
+
+      const thunk = fetchTradeData(comtrade, parameters);
+
+      comtrade.get.returns(Promise.resolve(comtradeData));
+
+      // Execute the async action
+      thunk(dispatch)
+        .then(() => {
+          // Assertions that the comtrade.get api is called correctly
+          assert(comtrade.get.calledOnce, 'comtrade.get is called once');
+          assert.equal(comtrade.get.args[0].length, 1, 'comtrade.get called with one argument');
+
+          const actualGetArg = comtrade.get.args[0][0];
+          assert.equal(actualGetArg, parameters, 'comtrade.get called with supplied parameters');
+
+          // Assertions that TRADE_DATA_REQUEST and TRADE_DATA_SUCCESS actions are dispatched.
+          assert(dispatch.calledTwice, 'dispatch is called twice');
+
+          assert.equal(dispatch.args[0].length, 1, 'first dispatch call has one argument');
+
+          const actualRequestAction = dispatch.args[0][0];
+          const expectedRequestAction = { type: TRADE_DATA_REQUEST, payload: undefined };
+
+          assert.deepEqual(
+            actualRequestAction,
+            expectedRequestAction,
+            'TRADE_DATA_REQUEST action is dispatched'
+          );
+
+          assert.equal(dispatch.args[1].length, 1, 'second dispatch call has one argument');
+
+          const actualSuccessAction = dispatch.args[1][0];
+          const expectedSuccessAction = {
+            type: TRADE_DATA_SUCCESS,
+            payload: {
+              dataset: 'test.dataset',
+              lastUpdated: Date.now()
+            }
+          };
+
+          assert.deepEqual(
+            actualSuccessAction,
+            expectedSuccessAction,
+            'TRADE_DATA_SUCCESS action is dispatched'
+          );
+        })
+        .then(() => clock.restore())
+        .then(done, done);
+    });
+
+    it('should manage event dispatching for failed trade data requests', (done) => {
+      const err = new Error('test.error');
+
+      const comtrade = { get: stub() };
+      const parameters = { tradeRegime: 'test.tradeRegime', partnerAreas: 'test.partnerAreas' };
+      const dispatch = spy();
+
+      const thunk = fetchTradeData(comtrade, parameters);
+
+      comtrade.get.returns(Promise.reject(err));
+
+      // Execute the async action
+      thunk(dispatch)
+        .then(() => {
+          // Assertions that the comtrade.get api is called correctly
+          assert(comtrade.get.calledOnce, 'comtrade.get is called once');
+          assert.equal(comtrade.get.args[0].length, 1, 'comtrade.get called with one argument');
+
+          const actualGetArg = comtrade.get.args[0][0];
+          assert.equal(actualGetArg, parameters, 'comtrade.get called with supplied parameters');
+
+          // Assertions that TRADE_DATA_REQUEST and TRADE_DATA_FAILURE actions are dispatched.
+          assert(dispatch.calledTwice, 'dispatch is called twice');
+
+          assert.equal(dispatch.args[0].length, 1, 'first dispatch call has one argument');
+
+          const actualRequestAction = dispatch.args[0][0];
+          const expectedRequestAction = { type: TRADE_DATA_REQUEST, payload: undefined };
+
+          assert.deepEqual(
+            actualRequestAction,
+            expectedRequestAction,
+            'TRADE_DATA_REQUEST action is dispatched'
+          );
+
+          assert.equal(dispatch.args[1].length, 1, 'second dispatch call has one argument');
+
+          const actualFailureAction = dispatch.args[1][0];
+          const expectedFailureAction = {
+            type: TRADE_DATA_FAILURE,
+            payload: err,
+            error: true
+          };
+
+          assert.deepEqual(
+            actualFailureAction,
+            expectedFailureAction,
+            'TRADE_DATA_FAILURE action is dispatched'
+          );
+        })
+        .then(done, done);
+    });
+  });
+
   describe('tradeDataReducer', () => {
     it('should return incoming state if it does not handle action.type', () => {
       const inState = 'test.inState';
