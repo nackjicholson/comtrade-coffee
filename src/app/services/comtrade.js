cc207e3cab498e88d6526da5528d07829be006a9
Will Vaughn <willieviseoae@gmail.com>
2016-02-21 11:44:26 -0800

    Test for making call to comtrade API

---
+++ src/app/services/comtrade.js
@@ -0,0 +1,14 @@
+import fetch from 'isomorphic-fetch';
+
+const comtrade = {
+  get(parameters) {
+    return fetch(`http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&cc=09&fmt=json&ps=2015&r=all&p=${parameters.partnerArea}&rg=${parameters.tradeRegime}`)
+      .then(req => req.json());
+  },
+
+  partnerAreas() {},
+
+  tradeRegimes() {}
+};
+
+export default comtrade;
---
+++ src/app/services/comtrade.test.js
@@ -0,0 +1,38 @@
+import assert from 'assert';
+import nock from 'nock';
+import comtrade from './comtrade';
+
+describe('app/services/comtrade', () => {
+  describe('get', () => {
+    it('should apply parameters to comtrade data api request for coffee', (done) => {
+      const coffeeData = { validation: 'test.validation', dataset: 'test.dataset' };
+
+      nock('http://comtrade.un.org')
+        .get('/api/get')
+        .query({
+          max: '50000',
+          type: 'C',
+          freq: 'A',
+          px: 'HS',
+          cc: '09', // coffee buzz buzz!
+          fmt: 'json',
+          ps: '2015',
+          r: 'all',
+          p: 'test.partnerArea', // from parameters
+          rg: 'test.tradeRegime' // from parameters
+        })
+        .reply(200, coffeeData);
+
+      const parameters = {
+        partnerArea: 'test.partnerArea',
+        tradeRegime: 'test.tradeRegime'};
+
+      comtrade
+        .get(parameters)
+        .then((actual) => {
+          assert.deepEqual(actual, coffeeData, 'returns json parsed coffee data');
+        })
+        .then(done, done);
+    });
+  });
+});
