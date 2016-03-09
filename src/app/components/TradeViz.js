e21aad99143876c0c460eef4bd4bd2599a069347
Will Vaughn <willieviseoae@gmail.com>
2016-03-08 22:26:16 -0800

    alter logic for displaying loader or viz

--- src/app/components/TradeViz.js
+++ src/app/components/TradeViz.js
@@ -3,39 +3,39 @@ import Griddle from 'griddle-react';
 import { Treemap } from 'react-d3';
 
 function TradeViz({ tradeData }) {
-  // The loader gif is a span that just takes up space by default.
-  let content = (
-    <div>
-      <Treemap
-        data={tradeData.data.valueTreemap}
-        width={1000}
-        height={300}
-        textColor="#484848"
-        title="Trade Value ($USD) by Partner 2015"
-      />
-      <Treemap
-        data={tradeData.data.quantityTreemap}
-        width={1000}
-        height={300}
-        textColor="#484848"
-        title="Trade Quantity (Kg) by Partner 2015"
-      />
-      <h4>Raw Data from Comtrade API</h4>
-      <Griddle
-        results={tradeData.data.raw}
-        resultsPerPage="50"
-        columns={['rtTitle', 'TradeValue', 'TradeQuantity']}
-      />
-      <p>Last Updated: {tradeData.lastUpdated && new Date(tradeData.lastUpdated).toISOString()}</p>
-    </div>
-  );
-
-  // But if the tradeData.isFetching show a spinning UN badge.
-  if (tradeData.isFetching) {
-    content = <img src="/images/un_loader.gif" width="60" height="60" />;
+  // Show the visualizations if data is not fetching
+  if (!tradeData.isFetching) {
+    return (
+      <div>
+        <Treemap
+          data={tradeData.data.valueTreemap}
+          width={1000}
+          height={300}
+          textColor="#484848"
+          title="Trade Value ($USD) by Partner 2015"
+        />
+        <Treemap
+          data={tradeData.data.quantityTreemap}
+          width={1000}
+          height={300}
+          textColor="#484848"
+          title="Trade Quantity (Kg) by Partner 2015"
+        />
+        <h4>Raw Data from Comtrade API</h4>
+        <Griddle
+          results={tradeData.data.raw}
+          resultsPerPage="50"
+          columns={['rtTitle', 'TradeValue', 'TradeQuantity']}
+        />
+        <p>
+          Last Updated: {tradeData.lastUpdated && new Date(tradeData.lastUpdated).toISOString()}
+        </p>
+      </div>
+    );
   }
 
-  return content;
+  // But if the tradeData is fetching show a spinning UN badge.
+  return <img src="/images/un_loader.gif" width="60" height="60" />;
 }
 
 export default TradeViz;
